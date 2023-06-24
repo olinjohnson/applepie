semi-fixed backprop code, still some matrix multiplication errors with layers of increasing size.

engine/models/mod2.py
@@ -8,12 +8,15 @@ class Layer:
         self.biases = np.random.randn(outputs)
 
     def calc(self, inputs):
-        # TODO: fix biases
         return np.dot(inputs, self.weights.T) + self.biases
 
     @staticmethod
     def activation_relu(inputs):
         return np.maximum(inputs, 0)
+
+    @staticmethod
+    def activation_leaky_relu(inputs):
+        return np.maximum(inputs, 0.01 * inputs)
 
     @staticmethod
     def activation_softmax(inputs):
@@ -30,13 +33,16 @@ class Layer:
         squares = (output - expected) ** 2
         return np.sum(squares) / len(expected)
 
-class OutputLayer(Layer):
-    def calc(self, inputs):
-        # TODO: fix biases
-        return np.dot(inputs, self.weights.T)
+def derivative_sigmoid(x):
+    return x * (1 - x)
 
+def derivative_relu(x):
+    return np.greater(x, 0) * 1
 
-LEARNING_RATE = 0.01
+def derivative_leaky_relu(x):
+    return np.where(x > 0, 1, 0.01)
+
+LEARNING_RATE = 0.2
 NUM_EPOCHS = 20000
 
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
@@ -45,12 +51,12 @@ expected = np.array([[0], [1], [1], [0]])
 
 model = [
     Layer(2, 2),
-    OutputLayer(2, 1)
+    Layer(2, 1)
 ]
 
 def forward_prop():
     z0 = model[0].calc(inputs)
-    a0 = Layer.activation_sigmoid(z0)
+    a0 = Layer.activation_leaky_relu(z0)
     z1 = model[1].calc(a0)
     a1 = Layer.activation_sigmoid(z1)
     loss = Layer.loss_mse(a1, expected)
@@ -62,18 +68,21 @@ def forward_prop():
 def back_prop(cache):
 
     dca1 = 2 * (cache["a1"] - expected)
-    da1z1 = np.multiply(cache["a1"], 1 - cache["a1"])
+    da1z1 = derivative_sigmoid(cache["a1"])
     dz1w1 = cache["a0"]
 
     dz1a0 = model[1].weights
-    da0z0 = np.multiply(cache["z0"], 1 - cache["z0"])
+    da0z0 = derivative_leaky_relu(cache["a0"])
     dz0w0 = inputs
+
+    dcb1 = np.mean(dca1 * da1z1, axis=0)
+    model[1].biases -= (dcb1 * LEARNING_RATE)
 
     dcw1 = np.mean(dca1 * da1z1 * dz1w1, axis=0)
     model[1].weights -= (dcw1 * LEARNING_RATE)
 
-    dcb1 = np.mean(dca1 * da1z1, axis=0)
-    model[0].biases -= (dcb1 * LEARNING_RATE)
+    dcb0 = np.mean(dca1 * da1z1 * dz1a0 * da0z0, axis=0)
+    model[0].biases -= (dcb0 * LEARNING_RATE)
 
     dcw0 = np.mean(dca1 * da1z1 * dz1a0 * da0z0 * dz0w0, axis=0)
     model[0].weights -= (dcw0 * LEARNING_RATE)
@@ -84,19 +93,21 @@ print("LOSS: ", l)
 # print("CACHE: \n", cache)
 losses = []
 for i in range(0, NUM_EPOCHS):
-    p = numpy.random.permutation(len(inputs))
-    inputs = inputs[p]
-    expected = expected[p]
+    # p = numpy.random.permutation(len(inputs))
+    # inputs = inputs[p]
+    # expected = expected[p]
     l, c = forward_prop()
     losses.append(l)
     back_prop(c)
 
 print("LOSS: ", l)
+inputs = [[1, 1], [0, 0], [1, 0], [0, 1]]
+expected = [[0], [0], [1], [1]]
+l, c = forward_prop()
+print(np.round(c["a1"]))
 
 plt.plot([x for x in range(0, len(losses))], [x for x in losses])
-plt.title("what the hell")
+plt.title("learning rate: " + str(LEARNING_RATE))
 plt.xlabel("num epochs")
 plt.ylabel("mean network MSE loss")
 plt.show()
-
-

engine/models/test.py
@@ -47,5 +47,5 @@ a2 = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
 
 a3 = np.array([[2], [3], [4], [5]])
 a4 = np.array([[1, 5], [2, 6], [3, 7], [4, 8]])
-p = np.random.permutation(len(a4))
+print(np.greater(a4, 4) * 1)
 